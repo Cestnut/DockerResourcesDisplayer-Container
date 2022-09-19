81feb174799aca95e53c02872832b96686f4ed9c
Admin Q4OS <adminq@q4os-desktop>
2022-09-19 14:12:32 +0200

    Network I/O is monitored as well. Timestamps are included in log files. Added a /containers route, which return a json with data

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 import docker 
 import json
 import os  
+import time
 
 def calculate_cpu_percent(cpu_stats, precpu_stats):
     cpu_count = cpu_stats["online_cpus"]
@@ -15,6 +16,15 @@ def calculate_cpu_percent(cpu_stats, precpu_stats):
 def calculate_memory_percent(memory_stats):
    return round(float(memory_stats['usage'])/float(memory_stats['limit'])*100,2)
 
+def calculate_network_in_out(container_stats):
+        conversion_costant = 1024
+        network_in = 0
+        network_out = 0 
+        for interface in container_stats['networks']:
+                network_in += container_stats["networks"][interface]['rx_bytes']
+                network_out += container_stats["networks"][interface]['tx_bytes']
+
+        return network_in/conversion_costant, network_out/conversion_costant
 
 app = Flask(__name__)
 
@@ -23,6 +33,21 @@ def index():
 
         return render_template('index.html')
 
+@app.route('/containers', methods=["GET"])
+def get_container():
+        id = request.args.get('id')
+        client = docker.from_env()
+        if id:
+                for container in client.containers.list():
+                        stats = container.stats(stream=False)
+                        print(id)
+                        if stats['id'] == id:
+                                print(id)
+                                return json.dumps(compute_container_stats(stats))
+                                break
+        else:
+                return json.dumps(all_containers_stats())
+
 @app.route('/table')
 def resource_table():
 
@@ -30,17 +55,38 @@ def resource_table():
         containers = []
         for container in client.containers.list():
                 container_stats = container.stats(stream=False)
-                name = container_stats['name']
-               	id = container_stats['id']
-               	cpu = calculate_cpu_percent(container_stats['cpu_stats'], container_stats['precpu_stats'])
-                memory = calculate_memory_percent(container_stats['memory_stats'])
-                containers.append({"id": id, "name": name, "cpu":cpu, "memory":memory})
-
-                file = open(os.path.join('./resource_logs', id), 'a')
-                file.write(str(cpu) + " " + str(memory)+ "\n")
-                file.close()
+                derived_container_stats = compute_container_stats(container_stats)
+                derived_container_stats['id'] = container_stats['id']
+                containers.append(derived_container_stats)
+                write_log(derived_container_stats)
 
         return render_template('resource_table.html', containers=containers)
+
+
+def write_log(container_stats):
+        file = open(os.path.join('./resource_logs', container_stats['id']), 'a')
+        file.write(str(time.time()) + " " + str(container_stats['cpu']) + " " + str(container_stats['memory']) \
+        + " " + str(container_stats['network_in']) + " " + str(container_stats['network_out']) + "\n")
+        file.close()
+
+def compute_container_stats(container_stats):
+        name = container_stats['name']
+       	cpu = calculate_cpu_percent(container_stats['cpu_stats'], container_stats['precpu_stats'])
+        memory = calculate_memory_percent(container_stats['memory_stats'])
+        network_in, network_out = calculate_network_in_out(container_stats) 
+
+        return dict({"name": name, "cpu":cpu, "memory":memory, "network_in":network_in, "network_out":network_out})
+
+#Different from the code in resource_table(), because here the return type is a dict where keys are id of containers.
+#In the main code of resource_table() the return type is a list where in every entry the id is a key on the same level as the others.
+def all_containers_stats():
+        client = docker.from_env()
+        containers = dict()
+        for container in client.containers.list():
+                container_stats = container.stats(stream=False)
+                derived_container_stats = compute_container_stats(container_stats)
+                containers[container_stats['id']] = derived_container_stats
+        return containers
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0")
